4133b55294d1d1b42c3edd2cb45a4c4bb809f3f9
Simon Gardling <titaniumtown@gmail.com>
2020-05-10 04:06:20 -0400

    minor stuff

--- pyfetch.py
+++ pyfetch.py
@@ -45,7 +45,7 @@ def misc_func():
     elif shell_name == "bash":
         shell_ver = environ['BASH_VERSION'].split('(')[0]
     if shell_ver != "":
-        shell = str(shell_name+" "+shell_ver)
+        shell = str(shell_name+" "+shell_ver).rstrip('\n')
     else:
         shell = shell_name
     
