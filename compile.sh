4fa4b5def370fae588d71d2b5f7e2f26ceb4422e
Simon Gardling <titaniumtown@gmail.com>
2020-05-12 14:44:42 -0400

    do stuff w/ compile.sh

--- compile.sh
+++ compile.sh
@@ -5,6 +5,6 @@ rm -frv pyfetch pyfetch_cpp pyfetch.c
 #C
 cython3 -3 --embed -o pyfetch.c pyfetch.py
 
-clang -O3 -mfpu=native -mcpu=native -I/usr/include/python3.8 -o pyfetch pyfetch.c -lpython3.8 -lpthread -lpthread -lm -lutil -ldl
+# clang -O3 -mfpu=native -mcpu=native -I/usr/include/python3.8 -o pyfetch pyfetch.c -lpython3.8 -lpthread -lpthread -lm -lutil -ldl
 
-# gcc -O3 -mtune=native -march=native -I/usr/include/python3.8 -o pyfetch pyfetch.c -lpython3.8 -lpthread
+gcc -O3 -mtune=native -march=native -I/usr/include/python3.8 -o pyfetch pyfetch.c -lpython3.8 -lpthread
