d89e37463a726aa55d93391f62976c414c6dd93c
Simon Gardling <titaniumtown@gmail.com>
2020-05-12 15:15:49 -0400

    remove unnecessary lines in compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-rm -frv pyfetch pyfetch_cpp pyfetch.c
+rm -frv pyfetch pyfetch.c
 
-
-#C
 cython3 -3 --embed -o pyfetch.c pyfetch.py
 
 # clang -O3 -mfpu=native -mcpu=native -I/usr/include/python3.8 -o pyfetch pyfetch.c -lpython3.8 -lpthread -lpthread -lm -lutil -ldl
