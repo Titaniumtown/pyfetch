bc825267a4a588a644b3bef9ae7cf698280098fb
neumattock <152253273+newmattock@users.noreply.github.com>
2024-05-03 15:20:14 -0700

    added macos + more features
    added macos support, color, as well as functionality for displaying the gpu and battery

--- logos.py
+++ logos.py
@@ -1,17 +1,46 @@
-logo_array = [['--------------------------------------',
-'--------------------------------------',
-'--------------------------------------',
-'---\\\\\\\\\\\\\\\\\\\\\\\\-----------------------',
-'----\\\\\\      \\\\\\----------------------',
-'-----\\\\\\      \\\\\\---------------------',
-'------\\\\\\      \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\------',
-'-------\\\\\\                    \\\\\\-----',
-'--------\\\\\\                    \\\\\\----',
-'---------\\\\\\        ______      \\\\\\---',
-'----------\\\\\\                   ///---',
-'-----------\\\\\\                 ///----',
-'------------\\\\\\               ///-----',
-'-------------\\\\\\////////////////------',
-'--------------------------------------',
-'--------------------------------------',
-'--------------------------------------']]
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    PINK = '\033[95m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+logo_array = [[bcolors.OKBLUE +'--------------------------------------',
+bcolors.OKBLUE + '--------------------------------------',
+bcolors.OKBLUE +'--------------------------------------',
+bcolors.OKBLUE +'---\\\\\\\\\\\\\\\\\\\\\\\\-----------------------',
+bcolors.OKBLUE +'----\\\\\\      \\\\\\----------------------',
+bcolors.OKBLUE +'-----\\\\\\      \\\\\\---------------------',
+bcolors.OKBLUE +'------\\\\\\      \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\------',
+bcolors.OKBLUE +'-------\\\\\\                    \\\\\\-----',
+bcolors.OKBLUE +'--------\\\\\\                    \\\\\\----',
+bcolors.OKBLUE +'---------\\\\\\        ______      \\\\\\---',
+bcolors.OKBLUE +'----------\\\\\\                   ///---',
+bcolors.OKBLUE +'-----------\\\\\\                 ///----',
+bcolors.OKBLUE +'------------\\\\\\               ///-----',
+bcolors.OKBLUE +'-------------\\\\\\////////////////------',
+bcolors.OKBLUE +'--------------------------------------',
+bcolors.OKBLUE +'--------------------------------------',
+bcolors.OKBLUE +'--------------------------------------'],
+[       bcolors.OKGREEN + "                    c.                 ",
+        bcolors.OKGREEN +"                ,xNMM.                 ",
+        bcolors.OKGREEN +"               .OMMMMo                 ",
+        bcolors.OKGREEN +"              OMMM0                    ",
+        bcolors.OKGREEN +"      .;loddo:: loolloddol;.           ",
+        bcolors.OKGREEN +"   cKMMMMMMMMMMNWMMMMMMMMMM0:          " ,
+        bcolors.YELLOW +"  .KMMMMMMMMMMMMMMMMMMMMMMMWd.         ",
+        bcolors.YELLOW +"  XMMMMMMMMMMMMMMMMMMMMMMMX.           ",
+        bcolors.RED +" ;MMMMMMMMMMMMMMMMMMMMMMMM:            ",
+        bcolors.RED +" :MMMMMMMMMMMMMMMMMMMMMMMM:            ",
+        bcolors.RED +" .MMMMMMMMMMMMMMMMMMMMMMMMX.            ",
+        bcolors.RED +"  kMMMMMMMMMMMMMMMMMMMMMMMMWd.         ",
+        bcolors.PINK +"  .XMMMMMMMMMMMMMMMMMMMMMMMMMMk        ",
+        bcolors.PINK +"   .XMMMMMMMMMMMMMMMMMMMMMMMMK.        ",
+        bcolors.OKBLUE +"    kMMMMMMMMMMMMMMMMMMMMMMd           ",
+        bcolors.OKBLUE +"      KMMMMMMMWXXWMMMMMMMk.            ",
+        bcolors.OKBLUE +"         .cooc,. .,coo:.               "]]
